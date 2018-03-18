5161b0538b4ac6cf7918881a365f6bd7c74872cf
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2018-03-18 22:08:09 +0200

    added the sun and venus and gave action to the sun

--- PlantsAR/ViewController.swift
+++ PlantsAR/ViewController.swift
@@ -31,19 +31,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     override func viewDidAppear(_ animated: Bool) {
-        let earthNode = SCNNode(geometry: SCNSphere(radius: 0.2))
-        earthNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Earth day") 
-        earthNode.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "Earth Specular")
-        earthNode.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "Earth Emission")
-        earthNode.geometry?.firstMaterial?.normal .contents = #imageLiteral(resourceName: "Earth Normal")
-        earthNode.position = SCNVector3(0,0,-1)
-        self.sceneView.scene.rootNode.addChildNode(earthNode)
+        let sunNode = SCNNode(geometry: SCNSphere(radius: 0.35))
+        let earthParent = SCNNode()
+         let venusParent = SCNNode()
+        sunNode.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun diffuse")
+        sunNode.position = SCNVector3(0,0,-1)
+        earthParent.position = SCNVector3(0,0,-1)
+        venusParent.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(sunNode)
+        self.sceneView.scene.rootNode.addChildNode(earthParent)
+        self.sceneView.scene.rootNode.addChildNode(venusParent)
+        let earthNode = planet(geometry: SCNSphere(radius: 0.2), diffuse: #imageLiteral(resourceName: "Earth day"), specular: #imageLiteral(resourceName: "Earth Specular"), emission: #imageLiteral(resourceName: "Earth Emission"), normal: #imageLiteral(resourceName: "Earth Normal"), position: SCNVector3(1.2,0,0))
+        let venusNode = planet(geometry: SCNSphere(radius: 0.1), diffuse: #imageLiteral(resourceName: "Venus Surface"), specular: nil, emission: #imageLiteral(resourceName: "Venus Atmosphere"), normal:nil, position: SCNVector3(0.7,0,0))
+        venusParent.addChildNode(venusNode)
+        earthParent.addChildNode(earthNode)
+        
         let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
         let forever = SCNAction.repeatForever(action)
-        earthNode.runAction(forever)
+        sunNode.runAction(forever)
         
     }
-    
+    func planet(geometry: SCNGeometry, diffuse: UIImage?,specular:UIImage?,emission: UIImage?,normal:UIImage?, position:SCNVector3) -> SCNNode {
+        let plantNode = SCNNode(geometry: geometry)
+                plantNode.geometry?.firstMaterial?.diffuse.contents = diffuse
+                plantNode.geometry?.firstMaterial?.specular.contents = specular
+                plantNode.geometry?.firstMaterial?.emission.contents = emission
+                plantNode.geometry?.firstMaterial?.normal .contents = normal
+                plantNode.position = position
+        
+        return plantNode
+    }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
